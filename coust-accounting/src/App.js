e0d78947cd9ff608ed9daabce865a6a8ed75fb0e
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2022-09-16 18:08:30 +0300

    Initialization Project

---
+++ coust-accounting/src/App.js
@@ -0,0 +1,45 @@
+import CostItem from "./components/CostItem";
+
+function App() {
+
+  const costs = [
+    {
+      date: new Date(2020, 2, 12),
+      description: "Холодильник",
+      amount: 999.99,
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: "MacBook",
+      amount: 1254.72,
+    },
+    {
+      date: new Date(2021, 3, 1),
+      description: "Джинсы",
+      amount: 49.99,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Начнем изучение React</h1>
+      <CostItem 
+        date={costs[0].date}
+        description={costs[0].description}
+        amount={costs[0].amount}
+      />
+      <CostItem 
+        date={costs[1].date}
+        description={costs[1].description}
+        amount={costs[1].amount}
+      />
+      <CostItem 
+        date={costs[1].date}
+        description={costs[1].description}
+        amount={costs[1].amount}
+      />
+    </div>
+  );
+}
+
+export default App;
